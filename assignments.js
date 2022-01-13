e0ea8e99cb6314de4a0af7c84e3616ccaf1334c2
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-13 16:05:48 -0600

    initial commit

---
+++ assignments.js
@@ -0,0 +1,45 @@
+
+
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+const country = 'United States of America';
+const continent = 'North America';
+let population = '330,000,000';
+
+console.log(country, continent, population);
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+const isIsland = false;
+const language = 'english';
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+
+
+// LECTURE: let, const and var
+// 1. Set the value of 'language' to the language spoken where you live (some
+// countries have multiple languages, but just choose one)
+// 2. Think about which variables should be const variables (which values will never
+// change, and which might change?). Then, change these variables to const.
+// 3. Try to change one of the changed variables now, and observe what happens
+
+// language = 'english'; //[Uncaught SyntaxError: Missing initializer in const declaration]
+
---
+++ script.js
@@ -0,0 +1,93 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+let PI = 3.14125
+
+//good variable names
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+//bad variable names
+let job1 = 'programmer';
+let job2 = 'teahcer';
+
+//console.log print statement
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// declaring a variable allows you to change information in multiple places at once
+
+//camelCase is the standard form of naming in JS
+
+// cannot start a variable name with an integer
+
+// variable names can only contain numbers, letters underscores, or the dollar sign.
+
+// variable names cannot be js reserved keywords (name is somewhat reserved)
+
+// variable names be lowercase unless declaring a class (OOP)
+
+// Variables in all caps are reserved for variables we know will be constant IE. let PI = 3.14125
+
+// variable names should be clear and accurate
+*/
+
+
+/*
+//boolean [returns: true]
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//'operator' introduced [returns: boolean]
+console.log(typeof true); 
+console.log(typeof javascriptIsFun); 
+console.log(typeof 23); 
+console.log(typeof 'Mike'); 
+
+//dyanmic typing example [returns: string]
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun); 
+
+//undefined [returns: undefined | undefined]
+let year;
+console.log(year);
+console.log(typeof year); 
+
+year = 1991;
+console.log(year);
+console.log(typeof year); 
+
+//null [returns: object???]
+console.log(typeof null);
+*/
+
+/*
+//reassigning a variable (mutating the age value)
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; 
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Allen'
+console.log(lastName)
+*/
+
+
+
+
+
